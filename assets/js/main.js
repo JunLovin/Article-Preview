58cfa35dc1f4efa424dc864c808d03095df7454a
Mathias Rendon <mathiassaid7@gmail.com>
2024-12-26 18:37:57 -0500

    Add classes for some divs to change it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 const shareBtn = document.getElementById('share')
 const shareOptions = document.getElementById('share-options')
+const shareContainer = document.getElementById('share-container')
+const paragraph = document.getElementById('paragraph')
+const authorContainer = document.getElementById('author')
 
 shareBtn.addEventListener('click', () => {
     shareOptions.classList.toggle('hidden')
+    shareBtn.classList.toggle('active')
+    paragraph.classList.toggle('absolute')
+    authorContainer.classList.toggle('margin')
+    shareContainer.classList.toggle('fullwidth')
 })
